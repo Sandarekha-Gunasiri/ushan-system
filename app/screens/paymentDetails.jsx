0ce52732e016716fbabd54f1fa7e50882f3c39e2
Sandarekha-Gunasiri <sandarekhagunasiri@gmail.com>
2024-05-07 01:25:03 -0700

    payment details API

--- app/screens/paymentDetails.jsx
+++ app/screens/paymentDetails.jsx
@@ -1,24 +1,74 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 
 const PaymentDetails = ({ route }) => {
   const { customerId } = route.params;
+  const [paymentData, setPaymentData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    fetchPaymentDetails();
+  }, []);
+
+  const fetchPaymentDetails = async () => {
+    try {
+      const response = await fetch(
+        `http://128.199.25.88:86/index.php/api/loan/loaninfo/${customerId}`
+      );
+      const responseData = await response.json();
+      console.log("Response data:", responseData);
+      setPaymentData(responseData);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error ", error);
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color="#00004B" />
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>
-        Payment Details for {customerId} 
-      </Text>
-      <View>
-        <Text style={styles.text}>Balance: </Text>
-        <Text style={styles.text}>Payment: </Text>
-        <Text style={styles.text}>Loan Start Date: </Text>
-        <Text style={styles.text}>Loan End Date: </Text>
-        <Text style={styles.text}>Amount:</Text>
-        <TextInput style={styles.input} placeholder="Enter Amount" />
-      </View>
+      <Text style={styles.header}>Payment Details for {customerId}</Text>
+      {paymentData && (
+        <View>
+          <Text style={styles.text}>
+            Customer:<Text style={styles.dataText}>{paymentData.customer}</Text>
+          </Text>
+
+          <Text style={styles.text}>
+            Balance:<Text style={styles.dataText}> {paymentData.balance}</Text>
+          </Text>
+          <Text style={styles.text}>
+            Payment:<Text style={styles.dataText}> {paymentData.payment}</Text>
+          </Text>
+          <Text style={styles.text}>
+            Loan Start Date:
+            <Text style={styles.dataText}>{paymentData.date}</Text>
+          </Text>
+          <Text style={styles.text}>
+            Loan End Date:
+            <Text style={styles.dataText}> {paymentData.duration}</Text>
+          </Text>
+          <Text style={styles.text}>
+            Amount:<Text style={styles.dataText}> {paymentData.amount}</Text>
+          </Text>
+          <TextInput style={styles.input} placeholder="Enter Amount" />
+        </View>
+      )}
       <TouchableOpacity style={styles.button}>
         <Text style={styles.buttonText}>BILL</Text>
       </TouchableOpacity>
@@ -27,6 +77,7 @@ const PaymentDetails = ({ route }) => {
 };
 
 export default PaymentDetails;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -58,23 +109,6 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 18,
   },
-  dtls: {
-    marginBottom: 10,
-    borderWidth: 1,
-    padding: 10,
-    borderRadius: 15,
-    borderColor: "#ffffff",
-    justifyContent: "space-around",
-    backgroundColor: "#ffffff",
-    width: "100%",
-    shadowColor: "#000",
-    shadowOpacity: 0.12,
-    shadowRadius: 8,
-    shadowOffset: {
-      width: 1,
-      height: 1,
-    },
-  },
   input: {
     height: 40,
     borderWidth: 1,
@@ -92,6 +126,14 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: "#00004B",
     marginHorizontal: 30,
-    marginVertical: 20,
+    marginVertical: 15,
+    fontWeight:"bold"
+  },
+  dataText: {
+    fontSize: 16,
+    color: "#00004B",
+    marginHorizontal: 30,
+    marginVertical: 15,
+    fontWeight:"normal"
   },
 });
